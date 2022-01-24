52f0f6b602f6d3b24dffd99779a9505463ca01f5
rajusundc <93384675+rajusundc@users.noreply.github.com>
2022-01-24 23:03:48 +0800

    main commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 <h2>What Can JavaScript Do?</h2>
 
 <p id="demo">JavaScript can change the style of an HTML element.</p>
+<p1 id="demo">JavaScript can change the style of an HTML element and thats right.</p1>
+
 
 <button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
 
